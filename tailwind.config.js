1f3a5a6e8d42f43391a9a099ccfe8825053af9dc
diyornv <diyorcode@gmail.com>
2025-02-18 01:23:24 +0500

    navbar section done

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                rubik: ["Rubik", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
