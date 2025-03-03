d2fea6236e695bcd0d7d693abbfe53e2f5d430f1
diyornv <diyorcode@gmail.com>
2025-03-03 18:55:02 +0500

    test prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    theme: {
-        extend: {
-            fontFamily: {
-                rubik: ["Rubik", "sans-serif"],
-            },
-        },
+  theme: {
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
